f76e04086b1c3e3b281d8a605e9c2f3ac507263c
Kyle Code <kyle.lane1992@yahoo.com>
2025-01-25 10:08:09 -0500

    updated about me

--- 12-React-Homework/src/Pages/AboutPage.jsx
+++ 12-React-Homework/src/Pages/AboutPage.jsx
@@ -1,6 +1,11 @@
 function AboutPage () {
     return(
+     <>
         <h1>About Page</h1>
+        <p>
+            I am a full stack Software Developer with a passion for building and learning. I have found Software Developing to be a great outlet for my creativity. I have been a Pipe fitter for the past 14 years and it has taught me a lot about time management, team work, and dedication.
+        </p>
+    </>
     )
 }
 export default AboutPage
