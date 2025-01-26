3e354ca7379a6d7c528b77b98ccbe690c79ef828
Kyle Code <kyle.lane1992@yahoo.com>
2025-01-26 11:14:05 -0500

    homework update

--- 12-React-Homework/src/Components/Footer/index.jsx
+++ 12-React-Homework/src/Components/Footer/index.jsx
@@ -1,6 +1,9 @@
 function Footer () {
     return(
-        <footer>footer</footer>
+        <footer>
+            <a href="https://github.com/laneman952" target="_blank" rel="noopener noreferrer"></a>
+            <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        </footer>
     )
 }
 export default Footer
