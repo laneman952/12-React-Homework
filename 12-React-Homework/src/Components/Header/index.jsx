a81cc7ff84f32d459280b40086f0a789903c37fc
laneman952 <kyle.lane1992@yahoo.com>
2025-01-27 22:39:40 -0500

    index.jsx, homepage.jsx, and app.css updated

--- 12-React-Homework/src/Components/Header/index.jsx
+++ 12-React-Homework/src/Components/Header/index.jsx
@@ -1,9 +1,12 @@
 // import headerImg from '../../assets/Header.jpg';
+
+import HomePage from "../../Pages/HomePage";
+
 // Add an image. Call it header.jpg and place it in the assets folder.
 function Header(props) { 
   return (
     <header>
-      <h1>Kyle Lane</h1>
+      <HomePage>Kyle Lane</HomePage>
       {/* <img src={headerImg}  alt="Header Image"></img> */}
       {props.children}
     </header>
--- 12-React-Homework/src/Pages/HomePage.jsx
+++ 12-React-Homework/src/Pages/HomePage.jsx
@@ -1,9 +1,10 @@
-import { useState, useEffect } from 'react';
+import { Link } from 'react-router';
 
-function HomePage () {
-    return(
-        <h1>Home Page</h1>
-    )
-}
+function HomePage (props) {
+    if (props.children === "Kyle Lane") {
+    return <h1> <Link to = "/">Kyle Lane</Link></h1>
+    } else {
+     return   <h1>Home Page</h1>
+}}
 
 export default HomePage
