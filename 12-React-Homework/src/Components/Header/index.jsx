b898a591b19e789f33e95ea26981c0568c8ea9f4
Kyle Code <kyle.lane1992@yahoo.com>
2025-01-30 19:59:25 -0500

    updated assets

--- 12-React-Homework/src/Components/Header/index.jsx
+++ 12-React-Homework/src/Components/Header/index.jsx
@@ -1,10 +1,10 @@
-// import headerImg from '../../assets/Header.jpg';
+import headerImg from '../../assets/Header.jpg';
 // Add an image. Call it header.jpg and place it in the assets folder.
 function Header(props) { 
   return (
     <header>
       <h1>Kyle Lane</h1>
-      {/* <img src={headerImg}  alt="Header Image"></img> */}
+      <img src={headerImg}  alt="Header Image"></img>
       {props.children}
     </header>
   );
