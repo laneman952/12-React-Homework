d6f2cf30752190cde52bd912b7b3af8468a2b5a1
laneman952 <kyle.lane1992@yahoo.com>
2025-02-03 00:10:17 -0500

    final updates to index.css, App.css, projects.js, index.jsx in the footer folder, and PortfolioPage.jsx

--- 12-React-Homework/src/Components/Footer/index.jsx
+++ 12-React-Homework/src/Components/Footer/index.jsx
@@ -1,8 +1,12 @@
 function Footer () {
     return(
         <footer>
-            <a href="https://github.com/laneman952" target="_blank" rel="noopener noreferrer">GitHub</a>
-            <a href="https://www.linkedin.com/in/kyle-lane-713483334/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+            <a href="https://github.com/laneman952" target="_blank" rel="noopener noreferrer">
+                <img src="../public/github-mark.png" alt="GitHub" className="footer-icon" />
+            </a>
+            <a href="https://www.linkedin.com/in/kyle-lane-713483334/" target="_blank" rel="noopener noreferrer">
+                <img src="../public/linkedin-logo.png" alt="Linkedin" className="footer-icon" />
+            </a>
         </footer>
     )
 }
--- 12-React-Homework/src/Pages/PortfolioPage.jsx
+++ 12-React-Homework/src/Pages/PortfolioPage.jsx
@@ -1,7 +1,23 @@
+import projects from "../data/projects";
+
 function PortfolioPage () {
     return(
+        <div className="portfolio-container">
         <h1>Portfolio</h1>
-    )
+        <div className="portfolio-grid">
+            {projects.map((project, index) => (
+                <div key={index} className="portfolio-item">
+                    <img src={project.image} alt={project.title} className="project-image" />
+                    <h2>{project.title}</h2>
+                    <p>{project.description}</p>
+                    <div className="project-links">
+                        <a href={project.github} target="_blank" rel="noopener noreferrer">GitHub</a>
+                    </div>
+                </div>
+            ))}
+        </div>
+    </div>
+    );
 }
 
 export default PortfolioPage
