8d52d0463effb7fbf3489c80c67e0773db1a933a
Kyle Code <kyle.lane1992@yahoo.com>
2025-02-21 20:35:07 -0500

    Updated app.css and project.js

--- 12-React-Homework/src/data/projects.js
+++ 12-React-Homework/src/data/projects.js
@@ -2,7 +2,7 @@ const projects = [
     {
         title: "Piano Pulse",
         image: "../public/firstproject.png",
-        description: "An interactive piano app. allows you to play piano, record music, and play back saved music.",
+        description: "An interactive piano app. Allows you to play piano, record music, and play back saved music.",
         github: "https://github.com/laneman952/firstProject.git"
     },
 ];
