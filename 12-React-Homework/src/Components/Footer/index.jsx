b548ff2f011c29901efec5545914f51a6bed8df8
laneman952 <kyle.lane1992@yahoo.com>
2025-02-24 22:40:07 -0500

    updated images

--- 12-React-Homework/src/Components/Footer/index.jsx
+++ 12-React-Homework/src/Components/Footer/index.jsx
@@ -2,10 +2,10 @@ function Footer () {
     return(
         <footer>
             <a href="https://github.com/laneman952" target="_blank" rel="noopener noreferrer">
-                <img src="../public/github-mark.png" alt="GitHub" className="footer-icon" />
+                <img src="/github-mark.png" alt="GitHub" className="footer-icon" />
             </a>
             <a href="https://www.linkedin.com/in/kyle-lane-713483334/" target="_blank" rel="noopener noreferrer">
-                <img src="../public/linkedin-logo.png" alt="Linkedin" className="footer-icon" />
+                <img src="/linkedin-logo.png" alt="Linkedin" className="footer-icon" />
             </a>
         </footer>
     )
