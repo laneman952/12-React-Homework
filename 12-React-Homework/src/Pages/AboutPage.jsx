0af2a54e14eedb167d57f192dc9b64f4dc142ebe
Kyle Code <kyle.lane1992@yahoo.com>
2025-04-13 17:42:58 -0400

    updated background image and fonts

--- 12-React-Homework/src/Pages/AboutPage.jsx
+++ 12-React-Homework/src/Pages/AboutPage.jsx
@@ -3,7 +3,12 @@ function AboutPage () {
      <>
         <h1>About Me</h1>
         <p>
-            I am a full stack Software Developer with a passion for building and learning. I have found Software Developing to be a great outlet for my creativity. I have been a Pipe fitter for the past 14 years and it has taught me a lot about time management, team work, and dedication.
+        I am a Full Stack Software Developer with a passion for learning and creating! 
+        As a recent graduate of UNC Chapel Hill, I am excited to begin my journey as a Software Developer. 
+        While I work well alone, I am also excited to join a team of other dedicated developers and collaborate on projects. 
+        I am a man of faith who loves his family. I bring a strong set of ethics, values, and character to the table, along with a fresh perspective and coding skill set. 
+        Creativity, time management, and effective communication skills are vital to what I bring into any work environment. 
+        I believe in setting the tone for working well with others and meeting both personal and professional goals. 
         </p>
     </>
     )
--- 12-React-Homework/src/Pages/HomePage.jsx
+++ 12-React-Homework/src/Pages/HomePage.jsx
@@ -1,10 +1,16 @@
 import { Link } from 'react-router';
 
-function HomePage (props) {
-    if (props.children === "Kyle Lane") {
-    return <h1> <Link to = "/">Kyle Lane</Link></h1>
-    } else {
-     return   <h1>Hello, I'm Kyle Lane. I am a Full Stack Software Developer!</h1>
-}}
+function HomePage(props) {
+  if (props.children === "Kyle Lane") {
+    return <h1><Link to="/">Kyle Lane</Link></h1>;
+  } else {
+    return (
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '100px' }}>
+        <h1>Hello, I'm Kyle Lane.</h1>
+        <h2>I am a Full Stack Software Developer!</h2>
+      </div>
+    );
+  }
+}
 
-export default HomePage
+export default HomePage;
