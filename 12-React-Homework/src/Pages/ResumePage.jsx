cc6699312067fc488bc3311479757d64fbc0ff2a
Kyle Code <kyle.lane1992@yahoo.com>
2025-05-06 14:28:33 -0400

    updated resumepage.jsx and it's css

--- 12-React-Homework/src/Pages/ResumePage.jsx
+++ 12-React-Homework/src/Pages/ResumePage.jsx
@@ -4,11 +4,13 @@ function ResumePage () {
     return(
         <div className="page-content">
         <h1>My Resume</h1>
+        <div className="resume-container">
         <iframe
             src="/Kyle's-Software-Developing-Resume.pdf"
             title="Kyle Lane Resume"
             className="resume-iframe"
         ></iframe>
+      </div>
         <p>
             <a href="/Kyle's-Software-Developing-Resume.pdf" download className="resume-link">
                 Download Resume (.docx)
